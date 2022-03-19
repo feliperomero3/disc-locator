97277b7253544a462fecf88af5f3ea52507eb7a3
Felipe Romero <feliperomero3@outlook.com>
2022-03-18 20:25:35 -0600

    Adding the controls templates.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   'use strict';
   console.log("ready!");
+  let $filters = $('#filters');
+  let $filterAdd = $('#filter-add');
   let movies;
   $.getJSON('movies.json', function (data) {
     movies = data;
@@ -8,32 +10,49 @@ $(document).ready(function () {
   });
 
   $(document).on('moviesLoaded', function () {
-    let $filters = $('#filters');
-    let $filterAdd = $('#filter-add');
+    setChangeHandler();
+
     let templatesAvailable = $('.template', '.templates').not('.filter-chooser').length;
 
-    $filterAdd.click(function () {
-      let selectedFilter = $filters.find('.template:last .filter-type').val();
-
-      if (selectedFilter === '') {
-        return;
-      }
-
-      let filterInUse = $filters.children().map(function () {
-        return $(this).children('.template').attr('class').match(/\b(template-.+?)\b/g)[0];
-      }).get();
-
-      if (filterInUse.length === templatesAvailable) {
-        return;
-      }
-
-      let $filterChooser = $('div.template.filter-chooser').clone().removeClass('filter-chooser').addClass('filter');
-
-      $filterChooser.find('option[data-template-type]').filter(function () {
-        return filterInUse.indexOf($(this).data('template-type')) >= 0;
-      }).remove();
-
-      $filterChooser.appendTo($filters);
-    }).click();
+    $filterAdd.click(setClickHandler($filters, templatesAvailable)).click();
   });
+
+  function setClickHandler($filters, templatesAvailable) {
+    let selectedFilter = $filters.find('.template:last .filter-type').val();
+
+    if (selectedFilter === '') {
+      return;
+    }
+
+    let filterInUse = $filters.children().map(function () {
+      let innerObj = $(this);
+      let childrenWithClass = $(this).children('.template');
+      return $(this).children('.template').attr('class').match(/\b(template-.+?)\b/g)[0];
+    }).get();
+
+    if (filterInUse.length === templatesAvailable) {
+      return;
+    }
+
+    let $filterChooser = $('div.template.filter-chooser').clone().removeClass('filter-chooser').addClass('filter');
+
+    $filterChooser.find('option[data-template-type]').filter(function () {
+      return filterInUse.indexOf($(this).data('template-type')) >= 0;
+    }).remove();
+
+    $filterChooser.appendTo($filters);
+
+  }
+
+  function setChangeHandler() {
+    $('#filters').on('change', '.filter-type', function () {
+      let $this = $(this);
+      let $filter = $this.closest('.filter');
+      let filterType = $this.find(':selected').data('template-type');
+
+      $('.qualifier', $filter).remove();
+      $('div.template.' + filterType).clone().addClass('qualifier').appendTo($filter);
+      $this.find('option[value=""').remove();
+    });
+  }
 });
