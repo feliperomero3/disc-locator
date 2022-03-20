bc6e9cf660e00af9ec70a97ffed59ef6669a8772
Felipe Romero <feliperomero3@outlook.com>
2022-03-19 20:06:48 -0600

    Showing the results.

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
   $(document).on('moviesLoaded', function () {
     setChangeHandler();
-    removeFilterHandler();
+    setRemoveFilterHandler();
 
     $filterAdd.click(function () {
       let selectedFilter = $filters.find('.template:last .filter-type').val();
@@ -40,6 +40,8 @@ $(document).ready(function () {
       $filterChooser.appendTo($filters);
 
     }).click();
+
+    setSubmitHandler();
   });
 
   function setChangeHandler() {
@@ -54,9 +56,68 @@ $(document).ready(function () {
     });
   }
 
-  function removeFilterHandler() {
+  function setRemoveFilterHandler() {
     $('#filters').on('click', '.filter-remover', function () {
       $(this).closest('.filter').remove();
+    });
+  }
+
+  function setSubmitHandler() {
+    $('#form-filters').submit(function (event) {
+      event.preventDefault();
+
+      let titleCondition = $filters.find('select[name="title-condition"]').val();
+      let title = $filters.find('input[name="title"]').val();
+      let binderMin = parseInt($filters.find('input[name="binder-min"]').val(), 10);
+      let binderMax = parseInt($filters.find('input[name="binder-max"]').val(), 10);
+      let yearMin = parseInt($filters.find('input[name="year-min"]').val(), 10);
+      let yearMax = parseInt($filters.find('input[name="year-max"]').val(), 10);
+      let viewed = $filters.find('input[name="viewed"]:checked').val();
+
+      $('tr:has(td)', '#results').remove();
+
+      let results = $.grep(movies, function (element) {
+        return ((
+          (
+            titleCondition === undefined &&
+            title === undefined
+          ) ||
+          (
+            titleCondition === 'contains' &&
+            element.title.indexOf(title) >= 0
+          ) ||
+          (
+            titleCondition === 'stars-with' &&
+            element.title.indexOf(title) === 0
+          ) ||
+          (
+            titleCondition === 'ends-with' &&
+            element.title.indexOf(title) === element.title.length - title.length
+          ) ||
+          (
+            titleCondition === 'equals' &&
+            element.title === title
+          )
+        ) &&
+          (isNaN(binderMin) || element.binder >= binderMin) &&
+          (isNaN(binderMax) || element.binder <= binderMax) &&
+          (isNaN(yearMin) || element.year >= yearMin) &&
+          (isNaN(yearMax) || element.year <= yearMax) &&
+          (viewed === undefined || element.viewed === (viewed === 'true'))
+        );
+      });
+
+      let row;
+      for (var i = 0; i < results.length; i++) {
+        row = '<td>' + results[i].title + '</td>';
+        row += '<td>' + results[i].year + '</td>';
+        row += '<td>' + results[i].binder + '</td>';
+        row += '<td>' + results[i].page + '</td>';
+        row += '<td>' + results[i].slot + '</td>';
+        row += '<td>' + (results[i].viewed ? 'X' : '') + '</td>';
+
+        $('#results').append($('<tr>').html(row));
+      }
     });
   }
 });
